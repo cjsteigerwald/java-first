09216fe7cbef6cc87b5123c37c0e1c7335f3ff0d
Steigerwald, Christopher (cs741q) <cs741q@att.com>
2022-12-06 00:31:50 -0800

    Adding start of project

---
+++ src/main/java/com/luv2code/com/MyApp.java
@@ -0,0 +1,9 @@
+package com.luv2code.com;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Coach theCoach = new TrackCoach();
+
+        System.out.print(theCoach.getDailyWorkout());
+    }
+}
